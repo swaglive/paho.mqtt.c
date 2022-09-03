b535085f178c6292082faefb6c37ae3713373379
yu840915 <yu840915@gmail.com>
2022-09-03 09:59:01 +0800

    fix

--- src/AsyncBridgeHelpers/MQTTHelperLinkingDummy.c
+++ src/AsyncBridgeHelpers/MQTTHelperLinkingDummy.c
@@ -8,6 +8,6 @@ __attribute__((visibility("default"))) __attribute__((used)) int MQTTHelper_link
   MQTTHelper_subscribe(NULL, NULL, atMostOnce);
   MQTTHelper_unsubscribe(NULL, NULL);
   MQTTHelper_disconnect(NULL);
-  MQTTHelper_connect(NULL, NULL, NULL, NULL);
+  MQTTHelper_connect(NULL, NULL, NULL, NULL, NULL, NULL);
   return 0;
 }
